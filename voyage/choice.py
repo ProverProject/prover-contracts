aec775a3ee10e652eb0f43d8be6438e3e103a4ad
blashyrkh <blashyrkh@inbox.ru>
2018-11-12 23:03:32 +0300

    Script to deterministically choose voyage winners

---
+++ voyage/choice.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys, binascii, bisect
+
+
+def choice(m, n, bignum):
+    assert m<=n+1
+    n+=1
+
+    r=[]
+    for i in range(m):
+        bignum,residue=divmod(bignum, n-i)
+        for j in range(i):
+            if residue>=r[j]:
+                residue+=1
+        # it's just an insert into sorted list keeping the list sorted
+        bisect.insort(r, residue)
+
+    return r
+
+
+if len(sys.argv)<4:
+    sys.exit("Usage: {0} M N HASH\nIt deterministically produces M distinct numbers from [0;N] using HASH as seed\n".format(sys.argv[0]))
+
+m=int(sys.argv[1])
+n=int(sys.argv[2])
+h=int("0x"+sys.argv[3], 16)
+
+print(choice(m, n, h))
